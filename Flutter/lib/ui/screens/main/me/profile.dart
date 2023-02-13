2ea0832db81b2c9c69ccd8fd5015ce1bc529765e
xichenhe <xichenhe@ucsb.edu>
2023-02-12 23:00:25 -0800

    new project creation

---
+++ Flutter/lib/ui/screens/main/me/profile.dart
@@ -0,0 +1,284 @@
+import 'package:algo_learn/ui/screens/screens.dart';
+import 'package:flutter/material.dart';
+import 'package:algo_learn/ui/screens/main/login/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:googleapis/admin/directory_v1.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:googleapis/cloudsearch/v1.dart';
+import 'package:algo_learn/theme/themes.dart';
+
+class MePage extends StatefulWidget {
+  const MePage({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _MePageState();
+}
+
+class _MePageState extends State<MePage> {
+  // var width = MediaQuery.of(context).size.width;
+  // var height = MediaQuery.of(context).size.height;
+
+  String username = 'aaaa';
+  String email = "aaaa";
+
+  int _selected_index = 0;
+  static const option_style = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selected_index = index;
+    });
+  }
+
+  Future<String> get_user_info() async {
+    email = FirebaseAuth.instance.currentUser!.email.toString();
+
+    await FirebaseFirestore.instance
+        .collection('login')
+        .get()
+        .then((QuerySnapshot querySnapshot) => {
+              querySnapshot.docs.forEach((doc) {
+                if (doc['email'] == email) {
+                  username = doc['name']!;
+                }
+              })
+            });
+    return username;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    get_user_info().then((value) => setState(() {
+          username = value;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Palette.accentColor,
+          title: Text(
+            'Profile',
+            style: TextStyle(
+              fontSize: 26,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
+              ))),
+      body: Center(
+        child: Padding(
+          // padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: EdgeInsets.only(top: 20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: Container(
+                    alignment: Alignment(0.0, 0.5),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://pbs.twimg.com/profile_images/1336703991124815907/FQIgoIHT_400x400.png"),
+                      radius: 60.0,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Text(
+                  username,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 10.0, left: 90.0),
+                      child: Container(
+                        child: Text(
+                          "Email: ",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 10.0, right: 70.0),
+                      child: Container(
+                        child: Text(
+                          email,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: OutlinedButton(
+                            onPressed: () {
+                              // jump to the wallet page
+                              Navigator.pushNamed(context, '/wallets');
+                              // Navigator.push(context, new MaterialPageRoute(
+                              //   builder: (context) =>
+                              //     new MainScreen())
+                              //   );
+                            },
+                            child: Text(
+                              "Wallets",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.black,
+                              side: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              minimumSize: Size(300, 40),
+                            ),
+                          ))),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: OutlinedButton(
+                            onPressed: () {
+                              // jump to the wallet page
+                              Navigator.pushNamed(context, '/mainScreen');
+                              // Navigator.push(context, new MaterialPageRoute(
+                              //   builder: (context) =>
+                              //     new MainScreen())
+                              //   );
+                            },
+                            child: Text(
+                              "Block Chain",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.black,
+                              side: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              minimumSize: Size(300, 40),
+                            ),
+                          ))),
+                ],
+              ),
+              // and a button to logout
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(top: 30.0),
+                      child: Container(
+                          alignment: Alignment.center,
+                          child: OutlinedButton(
+                            onPressed: () async {
+                              // firebase auth sign out
+
+                              await FirebaseAuth.instance.signOut();
+                              // print(_auth);
+                              // print('Signed out');
+                              // navigate to login page and change the route to login page
+                              Navigator.pushNamedAndRemoveUntil(
+                                  context, '/', (route) => false);
+                            },
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              primary: Colors.black,
+                              side: BorderSide(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              minimumSize: Size(300, 40),
+                            ),
+                          ))),
+                ],
+              ),
+
+              // Padding(
+              // //   padding: const EdgeInsets.only(top: 36.0, left: 150.0, right: 80.0),
+              // padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              //   child: Row(
+              //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //     children: [
+              //       const Text("Accounting days:",
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+              //       Expanded(
+              //         child: Container(),
+              //       ),
+              //       const Text("|",
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+              //       Expanded(
+              //         child: Container(),
+              //       ),
+              //       const Text("Number of Accounts :",
+              //           textAlign: TextAlign.center,
+              //           style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+              //       Expanded(
+              //         child: Container(),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+              // Expanded(
+              //     child: Container()
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Flutter/lib/ui/screens/main/wallet/wallet.dart
@@ -0,0 +1,139 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+// import 'package:googleapis/clouddebugger/v2.dart';
+import 'package:algo_learn/ui/screens/main/login/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:algo_learn/utils/my_button.dart';
+import 'package:algo_learn/utils/my_list.dart';
+import 'package:algo_learn/utils/my_algo.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:algo_learn/utils/my_nft.dart';
+import 'package:algo_learn/theme/themes.dart';
+
+class WalletsPage extends StatefulWidget {
+  @override
+  _WalletsPageState createState() => _WalletsPageState();
+}
+
+class _WalletsPageState extends State<WalletsPage> {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+            // padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16.0),
+            backgroundColor: Palette.accentColor,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Algo-wallet',
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 4.0),
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      color: Palette.accentColor,
+                    )),
+              ],
+            ),
+            leading: IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  color: Colors.white,
+                ))),
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(),
+            child: Column(children: [
+              SizedBox(height: 20),
+              Container(
+                height: 198,
+                child: PageView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller,
+                  children: [
+                    MyAlgo(
+                      balance: 895,
+                      cardNumber: 123456789,
+                      expiryMonth: 10,
+                      expiryYear: 24,
+                      color: Palette.accentColor,
+                    ),
+                    MyNFT(
+                      balance: 53,
+                      cardNumber: 123456789,
+                      expiryMonth: 12,
+                      expiryYear: 23,
+                      color: Colors.red,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 24),
+              SmoothPageIndicator(
+                controller: _controller,
+                count: 2,
+                effect: ExpandingDotsEffect(
+                  activeDotColor: Colors.grey.shade600,
+                ),
+              ),
+              SizedBox(height: 24),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    MyButton(
+                      iconImagePath: 'assets/images/credit.png',
+                      buttonText: 'Pay',
+                    ),
+                    MyButton(
+                      iconImagePath: 'assets/images/save_money.png',
+                      buttonText: 'Send',
+                    ),
+                    MyButton(
+                      iconImagePath: 'assets/images/bill.png',
+                      buttonText: 'Bills',
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 0),
+              Padding(
+                padding: const EdgeInsets.all(35.0),
+                child: Column(children: [
+                  MyListTile(
+                    iconImagePath: 'assets/images/transactions.png',
+                    tileTitle: 'History',
+                    tileSubtitle: 'Transactions Made',
+                  ),
+                  MyListTile(
+                    iconImagePath: 'assets/images/graph.png',
+                    tileTitle: 'Payments',
+                    tileSubtitle: 'Statistics on Payment',
+                  ),
+                ]),
+              )
+            ]),
+          ),
+        ));
+  }
+}

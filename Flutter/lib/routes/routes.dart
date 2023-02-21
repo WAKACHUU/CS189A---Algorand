b8882a27c632d6ab75a15b3fa2a7eb28ffd4d5ef
xichenhe <xichenhe@ucsb.edu>
2023-02-21 00:26:42 -0800

    backend first test

--- Flutter/lib/routes/routes.dart
+++
@@ -1,4 +0,0 @@
-export 'package:fluro/fluro.dart';
-
-export 'route_configurator.dart';
-export 'route_handlers.dart';
--- Flutter/lib/utils/crypto_utils.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:math';
-
-class CryptoUtils {
-  static String format(int amount, int decimals) {
-    return (amount / pow(10, decimals)).toStringAsFixed(2);
-  }
-}

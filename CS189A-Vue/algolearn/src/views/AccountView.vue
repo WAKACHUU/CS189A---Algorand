74227f41114b8f8166ab94c1cde2a00ee7998e47
wakachuu <yijiecai@ucsb.edu>
2023-03-11 15:28:57 -0800

    new account page adjustments; nft card component

--- CS189A-Vue/algolearn/src/views/AccountView.vue
+++ CS189A-Vue/algolearn/src/views/AccountView.vue
@@ -1,53 +1,89 @@
 <template>
-  <el-avatar size="large" shape="circle">
+  <el-avatar :size="414" class="top-avatar" shape="circle">
     <img src="@/assets/logo.png">
   </el-avatar>
-  <div>
-    <span>{{ username }} |</span>
-    <span>{{ algoAcc }} | </span>
-    <span>{{ useremail }}</span>
-  </div>
-  <div>
-  </div>
-  <el-tabs v-model="activeName" class="demo-tabs">
-    <el-tab-pane label="My Wallet" name="mywallet">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column label="Collection" width="210">
-          <template #default="scope">
-            <div style="display: flex; align-items: center">
-              <el-avatar><img src="@/assets/logo.png"></el-avatar>
-              <span style="margin-left: 10px">{{ scope.row.badgeName }}</span>
+  <div class="gray-back">
+    <div class="content-account">
+      <div style="background-color: white;">
+        <div class="account-basic">
+          <el-row>
+              <span class="account-username">{{ username }} </span>
+          </el-row>
+          <el-row>
+            <span class="account-algoacc">{{ algoAcc }} </span>
+          </el-row>
+          <el-row>
+            <span class="account-algoacc">{{ useremail }}</span>
+          </el-row>
+        </div>
+        <!-- tabs -->
+        <el-tabs 
+          v-model="activeName" 
+          class="account-tabs"
+          stretch="false"
+        >
+          <!-- my wallet -->
+          <el-tab-pane label="My Wallet" name="mywallet">
+            <el-table :data="NFTs" style="width: 100%">
+              <el-table-column label="Collection" width="210">
+                <template #default="scope">
+                  <div style="display: flex; align-items: center">
+                    <el-avatar><img src="@/assets/logo.png"></el-avatar>
+                    <span style="margin-left: 10px">{{ scope.row.name }}</span>
+                  </div>
+                </template>
+              </el-table-column>
+              <el-table-column prop="course" label="Course" width="100" />
+              <el-table-column label="Star Level" width="400">
+                  <template #default="scope">
+                    <StarLevel :starLevel="scope.row.starLevel"></StarLevel>
+                  </template>
+              </el-table-column>
+
+            </el-table>
+          </el-tab-pane>
+          <!-- collection -->
+          <el-tab-pane label="Collection" name="collection">
+            <div class="collection-div">
+              <div class="search">
+                <el-input 
+                  v-model="inputFilter" 
+                  placeholder="Please input"
+                  :prefix-icon="Filter"
+                  input-style="width: 1072px"
+                ></el-input>          
+              </div>
+              <div class="nft-cards">
+                <el-row>
+                  <el-col
+                    v-for="(item, index) in NFTs"
+                    :key="index"
+                    :span="8"
+                  >
+                    <NFTCard :NFTInfo="item"></NFTCard>
+                  </el-col>
+                </el-row>          
+              </div>
+
+              
             </div>
-          </template>
-        </el-table-column>
-        <el-table-column prop="value" label="Value" width="100" />
-        <el-table-column>
-            <template #default="scope">
-                <el-button type="text" @click="scope.row.isStarred = !scope.row.isStarred">
-                    <el-icon>
-                        <template v-if="scope.row.isStarred">
-                            <StarFilled />
-                        </template>
-                        <template v-else>
-                            <Star />
-                        </template>
-                    </el-icon>
-                </el-button>
-            </template>
-        </el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="Collection" name="collection">Collection</el-tab-pane>
-    <el-tab-pane label="Create NFT" name="transfer">
-      <ProfTransfer />
-    </el-tab-pane>
-  </el-tabs>
+          </el-tab-pane>
+          <el-tab-pane label="Create NFT" name="transfer">
+            <ProfTransfer />
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { Star, StarFilled } from '@element-plus/icons-vue'
+import { Star, StarFilled, Filter } from '@element-plus/icons-vue'
 import ProfTransfer from '@/components/ProfTransfer.vue'
+import NFTCard from '@/components/NFTCard.vue'
+import StarLevel from '@/components/StarLevel.vue'
 
 const username = ref('username')
 const algoAcc = ref('algoacc')
@@ -56,25 +92,82 @@ const useremail = ref('user@email.com')
 // tab controller
 const activeName = ref('mywallet')
 
-const tableData = [
+const inputFilter = ref('')
+const NFTs = ref([
   {
-    badgeName: 'lecture1 most active',
-    value: '2.207',
-    isStarred: true
+      name: ref("NFT 1"),
+      course: ref("CS189A"),
+      comment: ref("This is a comment"),
+      starLevel: ref(2),
+      imgSrc: ref("https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png")
   },
   {
-    badgeName: 'lecture1 most active',
-    value: '2.207',
-    isStarred: true
+    name: ref("NFT 1"),
+    course: ref("CS189A"),
+    comment: ref("This is a comment"),
+    starLevel: ref(2),
+    imgSrc: ref("https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png")
   },
   {
-    badgeName: 'lecture1 most active',
-    value: '2.207',
-    isStarred: true
+    name: ref("NFT 1"),
+    course: ref("CS189A"),
+    comment: ref("This is a comment"),
+    starLevel: ref(2),
+    imgSrc: ref("https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png")
   }
-]
+])
 
 </script>
 
 <style lang="less" scoped>
+.gray-back {
+  background-color: #F5F5F5;
+  height: 100%;
+}
+.content-account {
+  padding: 480px 20px 0px;
+  
+  // position: absolute;
+}
+.top-avatar {
+  margin: 169px 0 0 103px;
+  position: absolute;
+  z-index: 1000;
+}
+
+.account-basic {
+  padding-top: 120px;
+  padding-left: 106px;
+}
+.account-username {
+  font-size: 48px; 
+  font-family: Futura; 
+  font-weight: bold; 
+  width: 100%;
+  
+}
+
+.account-algoacc {
+  font-size: 21px; 
+  font-family: Futura; 
+  width: 100%;
+  line-height: 26px;
+}
+
+.account-tabs {
+  margin-top: 156px;
+}
+
+.collection-div {
+  width: 100%;
+  height: 100%;
+}
+.search {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.nft-cards {
+  padding: 37px;
+}
 </style>
--- CS189A-Vue/algolearn/src/views/SingleCourse.vue
+++ CS189A-Vue/algolearn/src/views/SingleCourse.vue
@@ -1,10 +1,18 @@
 <template>
- <NFTCard></NFTCard>
+ <NFTCard :NFTInfo="NFTInfo"></NFTCard>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup>    
 import NFTCard from '@/components/NFTCard.vue'
+import { ref } from 'vue'
 
+const NFTInfo = ref({
+    name: ref("NFT 1"),
+    course: ref("CS189A"),
+    comment: ref("This is a comment"),
+    starLevel: ref(2),
+    imgSrc: ref("https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png")
+})
 </script>
 
 <style lang="less" scoped>
